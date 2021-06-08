b860a3b1d8c32c6dd6458c72831f187c859dbf12
full_name <email_address>
2021-06-08 12:47:36 -0400

    Implement basic app with 'give feedback' and 'statistics' sections

--- src/App.js
+++ src/App.js
@@ -11,43 +11,20 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [allClicks, setAll] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [positive, setPositive] = useState(0);
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
 
   const handleGoodClick = () => {
-    setAll(allClicks + 1);
     setGood(good + 1);
-    setAverageValue(good, bad, allClicks);
-    setPositiveValue(good, allClicks);
-    // setAverage(average = (good - bad)/allClicks);
-    // setPositive(positive = (good/allClicks) * 100);
   }
 
   const handleNeutralClick = () => {
-    setAll(allClicks + 1);
     setNeutral(neutral + 1);
-    setAverageValue(good, bad, allClicks);
-    setPositiveValue(good, allClicks);
-    // setAverage(average = (good - bad)/allClicks);
-    // setPositive(positive = (good/allClicks) * 100);
   }
 
   const handleBadClick = () => {
-    setAll(allClicks + 1);
     setBad(bad + 1);
-    setAverageValue(good, bad, allClicks);
-    setPositiveValue(good, allClicks);
-    // setAverage((good - bad)/allClicks);
-    // setPositive((good/allClicks) * 100);
-  }
-
-  const setAverageValue = (good, bad, allClicks) => {
-    setAverage((good - bad)/allClicks);
-  }
-
-  const setPositiveValue = (good, allClicks) => {
-    setPositive((good/allClicks) * 100);
   }
 
   return (
@@ -60,7 +37,7 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {allClicks}</p>
+      <p>all {all}</p>
       <p>average {average}</p>
       <p>positive {positive} %</p>
     </div>
